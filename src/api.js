794dc409d8fe1a94dcaa24e80e2df491ff622061
Yashu2133 <rathikyashu1661@gmail.com>
2025-09-15 10:52:38 +0530

    frontend updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://password-reset-flow-gb7g.onrender.com/api/v2/auth", 
+  baseURL: "https://prf-backend-ck59.onrender.com/api/v2/auth", 
   withCredentials: true, 
 });
 export default API;
